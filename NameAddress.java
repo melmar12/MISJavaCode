9457b194d551f018b77b67277b00b1b76be40bf7
Melissa Martinez <melmar12@live.com>
2015-09-03 17:25:20 -0500

    first commit with code and notes

---
+++ NameAddress.java
@@ -0,0 +1,10 @@
+// Thu Aug 27 | ch 1
+public class NameAddress 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Melissa Martinez");
+		System.out.println("5414 Riverstone Crossing Drive");
+		System.out.println("Sugar Land, TX, 77479");
+	}
+}
---
+++ Time.java
@@ -0,0 +1,12 @@
+// Tue, Sep 1 | ch 2
+public class  Time 
+{
+	public static void main(String[] args) 
+	{
+		int minutes = 197;
+		int hours = minutes / 60;
+		int minutesLeftOver = minutes % 60;
+
+		System.out.println( minutes + " is eual to " + hours + " hours and " + minutesLeftOver + " minutes."); 	
+	}
+}
